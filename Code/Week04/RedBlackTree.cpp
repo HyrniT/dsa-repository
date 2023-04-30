9658e0245c31314a71daf869e76c4ccc6356f45b
Quang Hiển <ngqghien@gmail.com>
2023-05-01 01:32:03 +0700

    Update insert node in RBT

--- Code/Week04/RedBlackTree.cpp
+++ Code/Week04/RedBlackTree.cpp
@@ -2,8 +2,8 @@
 
 using namespace std;
 
-static int RED = 1;
-static int BLACK = 0;
+static bool RED = 1;
+static bool BLACK = 0;
 
 struct NODE
 {
@@ -11,13 +11,13 @@ struct NODE
     NODE *parent;
     NODE *left;
     NODE *right;
-    int color;
+    bool color;
 };
 
 // Khởi tạo một NODE từ một giá trị cho trước
-NODE* createNode(int data)
+NODE *createNode(int data)
 {
-    NODE* new_node = new NODE();
+    NODE *new_node = new NODE();
     new_node->data = data;
     new_node->parent = nullptr;
     new_node->left = nullptr;
@@ -42,39 +42,148 @@ void rotateLeft(NODE *&node)
     NODE *pX = node->right;
     NODE *pY = pX->left;
 
-    node->right = pX->left;
     pX->left = node;
+    node->right = pX->left;
 
     node->parent = pX;
-    if(pY != nullptr)
+    if (pY != nullptr)
         pY->parent = node;
-    
+
     node = pX;
 }
 
 void rotateRight(NODE *&node)
 {
-    NODE* nRoot = node->left;
-    node->left = nRoot->right;
-    nRoot->right = node;
-    node->parent = nRoot;
-    node = nRoot;
+    NODE *pX = node->left;
+    NODE *pY = pX->right;
+
+    pX->right = node;
+    node->left = pY;
+
+    node->parent = pX;
+    if (pY != nullptr)
+        pY->parent = node;
+
+    node = pX;
 }
 
-void Insert(NODE* &pRoot, int x)
+void Insert(NODE *&pRoot, int x)
 {
+    NODE *new_node = createNode(x);
+    NODE *prev_node = nullptr;
+    NODE *curr_node = new_node;
 
+    while (curr_node != nullptr)
+    {
+        prev_node = curr_node;
+        if (x < curr_node->data)
+            curr_node = curr_node->left;
+        else if (x > curr_node->data)
+            curr_node = curr_node->right;
+        else
+            return;
+    }
+
+    new_node->parent = prev_node;
+    if (prev_node == nullptr)
+        pRoot = new_node;
+    else if (x < prev_node->data)
+        prev_node->left = new_node;
+    else
+        prev_node->right = new_node;
+
+    fixInsert(new_node);
+}
+
+void fixInsert(NODE *new_node)
+{
+    NODE *parent_node = new_node->parent;
+    NODE *grandparent_node = nullptr;
+
+    while (parent_node != nullptr && parent_node->color == RED)
+    {
+        grandparent_node = parent_node->parent;
+
+        if(grandparent_node->left == parent_node)
+        {
+            NODE* uncle_node = grandparent_node->right;
+            if(uncle_node != nullptr && uncle_node->color == RED)
+            {
+                // Case 1: Uncle is RED
+                grandparent_node->color = RED;
+                parent_node->color = BLACK;
+                uncle_node->color = BLACK;
+
+                new_node = grandparent_node; // bắt đầu xét tiếp từ node grandparent
+                parent_node = new_node->parent;
+            }
+            else // nếu uncle đen (vì node null cũng là node đen nên không quan tâm có null hay không)
+            {
+                // Case 2: Uncle is BLACK
+                //             grandparent (...)
+                //     parent (R)              uncle (B)
+                // ...         new (R)     ...         ...
+                if(parent_node->right == new_node)
+                {
+                    new_node = parent_node; // gán để khi xoay cây xong new và parent không bị swap
+                    rotateLeft(new_node);
+                    parent_node = new_node->parent; // gán lại parent sau khi xoay
+                }
+                parent_node->color = BLACK;
+                grandparent_node->color = RED;
+                rotateRight(grandparent_node); 
+                //           grandparent (B)
+                //    new (R)              parent (R)
+                // ...    ...           ...         uncle (B)
+            }
+        }
+        else // grandparent_node->right == parent_node
+        {
+            NODE* uncle_node = grandparent_node->left;
+            if(uncle_node != nullptr && uncle_node->color == RED)
+            {
+                // Case 3: Uncle is RED
+                grandparent_node->color = RED;
+                parent_node->color = BLACK;
+                uncle_node->color = BLACK;
+
+                new_node = grandparent_node;
+                parent_node = new_node->parent;
+            }
+            else
+            {
+                // Case 4: Uncle is BLACK
+                //             grandparent (...)
+                //     uncle (B)              parent (R)
+                // ...         ...      new (R)         ...
+                if(parent_node->left == new_node)
+                {
+                    new_node = parent_node;
+                    rotateRight(new_node);
+                    parent_node = new_node->parent;
+                }
+                parent_node->color = BLACK;
+                grandparent_node->color = RED;
+                rotateLeft(grandparent_node);
+                //             grandparent (B)
+                //      parent (R)             new (R)
+                // unclde (B)    ...       ...         ...
+            }
+        }
+    }
+    // Ensure the root is BLACK
+    NODE* root = new_node;
+    while (root->parent != NULL) {
+        root = root->parent;
+    }
+    root->color = BLACK;
 }
 
 // Xóa một NODE với giá trị cho trước từ một cây RED-BLACK cho trước (Chú ý giá trị đó có tồn tại hay không)
-void Remove(NODE* &pRoot, int x)
+void Remove(NODE *&pRoot, int x)
 {
-
 }
 
-int main ()
+int main()
 {
-
 }
-
-
