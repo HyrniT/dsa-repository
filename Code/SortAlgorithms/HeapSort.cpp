c163e2c68e667973de28d9394cdf484f3390b2d5
Quang Hiển <ngqghien@gmail.com>
2023-05-11 11:36:33 +0700

    Fix heap sort

--- Code/SortAlgorithms/HeapSort.cpp
+++ Code/SortAlgorithms/HeapSort.cpp
@@ -6,7 +6,7 @@ void heapify(int arr[], int n, int i)
 {
     int largest = i;
     int left = i * 2 + 1;
-    int right = i * 2 + 1;
+    int right = i * 2 + 2;
     if (left < n && arr[left] > arr[largest])
         largest = left;
     if (right < n && arr[right] > arr[largest])
