02b0ae2cc7a33a0054b848b33665315cbc767e54
Quang Hiển <ngqghien@gmail.com>
2023-05-18 23:47:08 +0700

    Update lab 5 to 7

--- Code/Graph/Dijkstra.cpp
+++ Code/Graph/Dijkstra.cpp
@@ -1,4 +1,26 @@
 #include <iostream>
+#include<vector>
 
 using namespace std;
 
+class Graph
+{
+private:
+    int V;
+    vector< vector<int> > adjList;
+
+public:
+    Graph(int vertices)
+    {
+        V = vertices;
+        adjList.resize(V);
+    }
+
+    void addEdge(int v, int w)
+    {
+        adjList[v].push_back(w);
+        adjList[w].push_back(v);
+    }
+
+    
+};
