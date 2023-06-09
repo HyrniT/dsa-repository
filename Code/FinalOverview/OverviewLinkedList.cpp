1a05488a0035c9c47ace34ea6ffb7df26f345f54
Quang Hiển <ngqghien@gmail.com>
2023-06-09 20:41:50 +0700

    Update overview linked list

--- Code/FinalOverview/OverviewLinkedList.cpp
+++ Code/FinalOverview/OverviewLinkedList.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-struct Node 
+struct Node
 {
     int key;
     Node* next;
@@ -19,9 +19,9 @@ Node* createNode(int key)
 Node* addHead(Node* head, int key)
 {
     Node* newNode = createNode(key);
-    if(head == nullptr)
+    if (head == nullptr)
         return newNode;
-    else 
+    else
     {
         newNode->next = head;
         head = newNode;
@@ -74,6 +74,12 @@ Node* delHead(Node* head)
 {
     if (head == nullptr)
         return nullptr;
+    else if (head->next == nullptr)
+    {
+        delete head;
+        head = nullptr;
+        return nullptr;
+    }
     else
     {
         Node* currNode = head;
@@ -88,6 +94,12 @@ Node* delTail(Node* head)
 {
     if (head == nullptr)
         return nullptr;
+    else if (head->next == nullptr)
+    {
+        delete head;
+        head = nullptr;
+        return nullptr;
+    }
     else
     {
         Node* prevNode = nullptr;
@@ -132,6 +144,33 @@ Node* delAt(Node* head, int pos)
     return head;
 }
 
+void delAll(Node* &head)
+{
+    while (head != nullptr)
+    {
+        //head = delHead(head);
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+        temp = nullptr;
+    }
+}
+
+int find(Node* head, int key)
+{
+    int count = 0;
+    Node* currNode = head;
+    while (currNode != nullptr)
+    {
+        if (currNode->key == key)
+            return count;
+        currNode = currNode->next;
+        count++;
+    }
+    if (currNode == nullptr)
+        return -1;
+}
+
 Node* reverse(Node* head)
 {
     if (head == nullptr || head->next == nullptr)
@@ -142,17 +181,76 @@ Node* reverse(Node* head)
     return p;
 }
 
-Node* concat(Node* head1, Node* head2)
+Node* merge(Node* head1, Node* head2)
 {
-    Node* newNode = new Node();
-    
+    if (head1 == nullptr)
+        return head2;
+    if (head2 == nullptr)
+        return head1;
+
+    Node* newNode = nullptr;
+    Node* currNode1 = head1;
+    Node* currNode2 = head2;
+
+    while (currNode1 != nullptr && currNode2 != nullptr)
+    {
+        if (currNode1->key < currNode2->key)
+        {
+            newNode = addTail(newNode, currNode1->key);
+            currNode1 = currNode1->next;
+        }
+        else
+        {
+            newNode = addTail(newNode, currNode2->key);
+            currNode2 = currNode2->next;
+        }
+    }
+
+    while (currNode1 != nullptr)
+    {
+        newNode = addTail(newNode, currNode1->key);
+        currNode1 = currNode1->next;
+    }
+
+    while (currNode2 != nullptr)
+    {
+        newNode = addTail(newNode, currNode2->key);
+        currNode2 = currNode2->next;
+    }
+
+    return newNode;
+}
+
+Node* concatenate(Node* head1, Node* head2)
+{
+    if (head1 == nullptr)
+        return head2;
+    if (head2 == nullptr)
+        return head1;
+
+    Node* currNode1 = head1;
+    Node* currNode2 = head2;
+    Node* newNode = nullptr;
+
+    while (currNode1->next != nullptr)
+    {
+        newNode = addTail(newNode, currNode1->key);
+        currNode1 = currNode1->next;
+    }
+
+    while (currNode2->next != nullptr)
+    {
+        newNode = addTail(newNode, currNode2->key);
+        currNode2 = currNode2->next;
+    }
+
     return newNode;
 }
 
 void print(Node* head)
 {
     Node* currNode = head;
-    while(currNode != nullptr)
+    while (currNode != nullptr)
     {
         cout << currNode->key << " ";
         currNode = currNode->next;
@@ -167,16 +265,32 @@ int main()
     LinkedList = addHead(LinkedList, 4);
     LinkedList = addTail(LinkedList, 2);
     LinkedList = addTail(LinkedList, 1);
-    
+
     // LinkedList = delHead(LinkedList);
     // LinkedList = delTail(LinkedList);
-    
+
     LinkedList = addAt(LinkedList, 5, 4);
     LinkedList = delAt(LinkedList, 4);
-    
+
     print(LinkedList);
     LinkedList = reverse(LinkedList);
     print(LinkedList);
-    
+
+    Node* LinkedList1 = nullptr;
+    LinkedList1 = addTail(LinkedList1, 5);
+    LinkedList1 = addTail(LinkedList1, 6);
+    LinkedList1 = addTail(LinkedList1, 7);
+    print(LinkedList1);
+
+    Node* LinkedList2 = nullptr;
+    LinkedList2 = concatenate(LinkedList, LinkedList1);
+    LinkedList2 = delTail(LinkedList2);
+    print(LinkedList2);
+
+    cout << find(LinkedList2, 7) << endl;
+
+    delAll(LinkedList2);
+    print(LinkedList2);
+
     return 0;
 }
